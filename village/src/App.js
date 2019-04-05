c8fd11abaeddc49c0e7d1a0db64dfd2497b0e644
Timothy Shores <timothymshores@gmail.com>
2019-04-05 10:31:38 -0500

    Create axios GET request to retrieve smurfs array from server and update state in App.js

--- village/src/App.js
+++ village/src/App.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
-
-import './App.css';
+import axios from 'axios';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
-    return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            smurfs: [],
+        };
+    }
+
+    // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+    // Notice what your map function is looping over and returning inside of Smurfs.
+    // You'll need to make sure you have the right properties on state and pass them down to props.
+
+    componentDidMount() {
+        axios
+            .get('http://localhost:3333/smurfs')
+            .then(response => this.setState({ smurfs: response.data }))
+            .catch(err => console.log(err));
+    }
+    render() {
+        return (
+            <div className="App">
+                <SmurfForm />
+                <Smurfs smurfs={this.state.smurfs} />
+            </div>
+        );
+    }
 }
 
 export default App;
